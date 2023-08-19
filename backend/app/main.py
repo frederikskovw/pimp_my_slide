47e265c38a53043fae23835bf77c25a0ecab9e9e
Frederik Skov Wacher <fsw@blazarcapital.com>
2023-08-19 19:11:02 +0200

    added logs

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,17 +5,20 @@ from dotenv import load_dotenv
 from models import FixSlides
 from templates import FIX_LEAD_IN_TEMPLATE, FIX_BODY_TEMPLATE, REVIEW_DATA_TEMPLATE, REVIEW_HORIZONTAL_FLOW_TEMPLATE, REVIEW_VERTICAL_FLOW_TEMPLATE, WRITE_EXECSUM_TEMPLATE
 from docx import Document
+import logging
 import io
 import json
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://main--teal-conkies-5d8062.netlify.app"}})
+logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 @app.route('/fix-slides', methods=['POST'])
 def fix_slides():
+    logging.info("Received a request at /fix-slides")
     if 'wordDoc' in request.files:
         print("Word doc found")
         word_file = request.files['wordDoc']
@@ -30,33 +33,37 @@ def fix_slides():
             slides_string += f"Lead in: < {slide['leadIn']} >\n"
             slides_string += f"Body: < {slide['content']} >\n"
             slides_string += f"Data: < {slide['data']} >\n\n"
-    
-    print(slides_string)
 
     use_gpt4 = request.form['useGPT4'] == "true"
     model_name = "gpt-4" if use_gpt4 else "gpt-3.5-turbo-16k"
-    print("Using model: " + model_name)
+    logging.info("Using model: " + model_name)
 
     # Rewrite slides
     fix_slides_instance = FixSlides(OPENAI_API_KEY, model_name)
 
+    logging.info("Calling model to fix body")
     slides_string_updated_body = fix_slides_instance.call_model(slides_string, FIX_BODY_TEMPLATE)
+    logging.info("Calling model to fix lead in")
     slides_string_updated_body_and_lead_ins = fix_slides_instance.call_model(slides_string_updated_body, FIX_LEAD_IN_TEMPLATE)
+    logging.info("Calling model to fix data")
     slides_string_updated_body_and_lead_ins_and_review_data = fix_slides_instance.call_model(slides_string_updated_body_and_lead_ins, REVIEW_DATA_TEMPLATE)
 
     return jsonify(slides_string_updated_body_and_lead_ins_and_review_data)
 
 @app.route('/review-flow', methods=['POST'])
 def review_flow():
+    logging.info("Received a request at /review-flow")
     updated_slides = json.loads(request.form['slides'])
     use_gpt4 = request.form['useGPT4'] == "true"
 
     model_name = "gpt-4" if use_gpt4 else "gpt-3.5-turbo-16k"
-    print("Using model: " + model_name)
+    logging.info("Using model: " + model_name)
 
     fix_slides_instance = FixSlides(OPENAI_API_KEY, model_name)
 
+    logging.info("Calling model to review horizontal flow")
     horizontal_flow_review = fix_slides_instance.call_model(updated_slides, REVIEW_HORIZONTAL_FLOW_TEMPLATE)
+    logging.info("Calling model to review vertical flow")
     vertical_flow_review = fix_slides_instance.call_model(updated_slides, REVIEW_VERTICAL_FLOW_TEMPLATE)
 
     return jsonify({
@@ -66,13 +73,16 @@ def review_flow():
 
 @app.route('/write-execsum', methods=['POST'])
 def write_execsum():
+    logging.info("Received a request at /write-execsum")
     updated_slides = json.loads(request.form['slides'])
     use_gpt4 = request.form['useGPT4'] == "true"
 
     model_name = "gpt-4" if use_gpt4 else "gpt-3.5-turbo-16k"
-    print("Using model: " + model_name)
+    logging.info("Using model: " + model_name)
 
     fix_slides_instance = FixSlides(OPENAI_API_KEY, model_name)
+
+    logging.info("Calling model to write exec sum")
     exec_sum = fix_slides_instance.call_model(updated_slides, WRITE_EXECSUM_TEMPLATE)
 
     return jsonify(exec_sum)
