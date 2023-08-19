12a6866b9fa6817d3478c415eb13bb9de45d253c
Frederik Skov Wacher <fsw@blazarcapital.com>
2023-08-19 15:43:34 +0200

    pointing to backend in heroku app

--- frontend/src/composables/SlidesToBackend.js
+++ frontend/src/composables/SlidesToBackend.js
@@ -8,7 +8,7 @@ const sendSlidesToBackend = async (endpoint, slides, useGPT4, wordDoc=null) => {
         formData.append('slides', JSON.stringify(slides));
     }
     formData.append('useGPT4', useGPT4);
-    const response = await axios.post("http://127.0.0.1:5000/" + endpoint, formData, {
+    const response = await axios.post("https://pimp-my-slide-backend.herokuapp.com/" + endpoint, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
