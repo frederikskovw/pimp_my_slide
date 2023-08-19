a64a24580f4ab12d7886447f057f70455cb55073
Frederik Skov Wacher <fsw@blazarcapital.com>
2023-08-19 14:55:05 +0200

    intial commit for heroku deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -78,4 +78,4 @@ def write_execsum():
     return jsonify(exec_sum)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
