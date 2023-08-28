f320ad4f699934c26a46782ae64742faba37f933
frederikskovw <97087128+frederikskovw@users.noreply.github.com>
2023-08-28 22:39:22 +0200

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Navigate to backend and set it up
 cd backend
 pip install -r requirements.txt
 python app/main.py &
 
-# Wait for a few seconds to ensure backend starts up
 sleep 5
 
-# Navigate to frontend and set it up
 cd ../frontend
 npm install
 npm run serve
 
-# Open the application in a web browser (optional)
 open http://localhost:8080
